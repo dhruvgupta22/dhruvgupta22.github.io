1c605cd9512d42eff11af68f02443152d4d3bd22
Dhruv <dhruvgupta22@iitk.ac.in>
2024-11-24 23:27:09 +0530

    import fix8

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1732470740',resources: [
+    cacheName: 'chirpy-1732471017',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
